87fde09664cc895c878d27250d91500c0fa451ef
Paritosh-J <paritoshj2001@gmail.com>
2025-09-17 12:35:08 +0530

    Dev - Room class definition

--- backend/src/main/java/com/paritosh/webinge/model/Room.java
+++ backend/src/main/java/com/paritosh/webinge/model/Room.java
@@ -1,4 +1,28 @@
 package com.paritosh.webinge.model;
 
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Data
 public class Room {
+
+    private final String roomId;
+    private final Map<String, Member> members = new ConcurrentHashMap<>();
+    private final Instant createdAt = Instant.now();
+
+    public Room(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public void addMember(Member member) {
+        members.put(member.getSessionId(), member);
+    }
+
+    public void removeMember(String sessionId) {
+        members.remove(sessionId);
+    }
+
 }
